b06b6978903fc78a5f53c796031f372012d64498
Min-Chang-Kyeoung <janu723@naver.com>
2020-08-11 21:15:30 +0900

    Initial Commit

---
+++ GamePartner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GamePartner
+//
+//  Created by 민창경 on 2020/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
