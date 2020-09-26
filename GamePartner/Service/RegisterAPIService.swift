f28333dee53365128e34f1b6980a3482a3602e5f
Min-Chang-Kyeoung <janu723@naver.com>
2020-09-26 21:43:39 +0900

    complete user Service API

--- GamePartner/Service/RegisterAPIService.swift
+++ GamePartner/Service/RegisterAPIService.swift
@@ -16,6 +16,8 @@ class RegisterAPIService : NSObject{
     
     // MARK: - URL
     private var checkUrl = "https://dapi.kakao.com/v2/vision/adult/detect"
+    private var uploadUrl = "http://34.105.28.130:7940/api/v1/uploadImage/post"
+    private var insertUserUrl = Util.mainUrl + Util.insertUserUrl
     // MARK: - Services
     func checkImage(image:UIImage!,userId:String!,imageType:String!) -> Promise<[String:Any]>{
         return Promise { seal in
@@ -41,5 +43,61 @@ class RegisterAPIService : NSObject{
         }
     }
     
+    func insertImage(image:UIImage!,userId:String!,imageType:String!) -> Promise<[String:Any]>{
+        return Promise{ seal in
+            AF.upload( multipartFormData: { multiformData in
+                
+                multiformData.append(image!.jpegData(compressionQuality: 0.5)!, withName: "image"
+                                    ,fileName: userId + imageType, mimeType: "image/" + imageType)
+                        
+            },to:uploadUrl,method: .post)
+            .validate()
+            .responseJSON{ response in
+                switch response.result {
+                case .success(let json):
+                    guard let json = json  as? [String: Any] else {
+                        return seal.reject(AFError.responseValidationFailed(reason: .dataFileNil))
+                    }
+                    seal.fulfill(json)
+                case .failure(let error):
+                    seal.reject(error)
+                }
+            }
+        }
+    }
+    
+    func insertUser(userId:String, pw:String, sex:String, age:Int,
+                    birthDay:String, favoritGame:String, introduce: String
+                    ,nickName:String, imgPath:String) -> Promise<[String:Any]>{
+        
+        let params = [
+            "userId":userId,
+            "pw":pw,
+            "sex":sex,
+            "age":age,
+            "birthDay":birthDay,
+            "favoritGame":favoritGame,
+            "introduce":introduce,
+            "nickName":nickName,
+            "imgPath":imgPath
+        ] as [String : Any]
+
+        return Promise{ seal in
+            
+        AF.request(insertUserUrl, method: .post, parameters: params)
+            .validate()
+            .responseJSON{ response in
+                switch response.result{
+                case .success(let json):
+                    guard let json = json  as? [String: Any] else {
+                        return seal.reject(AFError.responseValidationFailed(reason: .dataFileNil))
+                    }
+                    seal.fulfill(json)
+                case .failure(let error):
+                    seal.reject(error)
+                }
+            }
+        }
+    }
 }
 
---
+++ GamePartner/Util/Util.swift
@@ -0,0 +1,13 @@
+//
+//  Util.swift
+//  GamePartner
+//
+//  Created by 민창경 on 2020/09/24.
+//
+
+class Util {
+    static var mainUrl = "https://ecoa4rz9zk.execute-api.us-east-1.amazonaws.com/dev/"
+    static var userMainUrl = "http://34.105.28.130:7940/api/v1/uploadImage/"
+    
+    static var insertUserUrl = "user/insertUser"
+}
