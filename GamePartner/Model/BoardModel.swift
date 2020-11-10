f385854a3dce9ccce4c33e71bff012f84e0f064a
Min-Chang-Kyeoung <janu723@naver.com>
2020-11-10 23:33:51 +0900

    board model 수정

--- GamePartner/Model/BoardModel.swift
+++ GamePartner/Model/BoardModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 struct BoardModel {
     var imgPath:String
+    let boardOid:String
     let userId:String
     let favoritGame:String
     let nickName:String
@@ -17,7 +18,7 @@ struct BoardModel {
     let clickCount:Int
     
     init(imgPath:String, userId:String, favoritGame:String, nickName:String, age:String
-         , date:String, clickCount:Int) {
+         , date:String, clickCount:Int, boardOid:String) {
         self.nickName = nickName
         self.userId = userId
         self.favoritGame = favoritGame
@@ -25,5 +26,6 @@ struct BoardModel {
         self.imgPath = imgPath
         self.date = date
         self.clickCount = clickCount
+        self.boardOid = boardOid
     }
 }
